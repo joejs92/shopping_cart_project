e728cffe1dcf43240d276b5cd0a50958467fa593
joejs92 <joejs_92@yahoo.com>
2024-11-08 18:48:48 -0500

    add Home and Shopt components

---
+++ src/Home.jsx
@@ -0,0 +1,8 @@
+function Home(){
+
+    return(
+    <>
+        <h2>Welcome to the most crap-tastic store on the web!</h2>
+    </>
+    )
+}
---
+++ src/Shop.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+
+function Shop(){
+    const [items, setItems] = useState([]);
+    useEffect(()=>{
+        fetch('https://fakestoreapi.com/products')
+        .then(res=>res.json())
+        .then(json=>setItems(json))
+        .catch((error => console.error(error)));
+    }, []);
+  
+
+  return (
+    <>
+      <div className='shopping'>
+          {items.map((item)=>{
+            return <Card key = {item.id} image = {item.image} title = {item.title} price = {item.price}/>
+          })}
+      </div>
+    </>
+  )
+}
+
+export default Shop;
